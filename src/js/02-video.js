4db9073f043648be3dea254dde4207567d35fa40
VasylievYurii <uraplanetianin@gmail.com>
2023-05-10 16:48:29 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
 
+const iframeRef = document.querySelector('iframe');
+const iframeVimeoPlayer = new Vimeo(iframeRef);
+
+iframeVimeoPlayer.on(
+  'timeupdate',
+  throttle(() => {
+    iframeVimeoPlayer
+      .getCurrentTime()
+      .then(seconds => {
+        localStorage.setItem('videoplayer-current-time', seconds);
+      })
+      .catch(function (error) {
+        console.error('Error! Didn`t get current time', error);
+      });
+  }, 1000)
+);
+
+const playbackPosition = localStorage.getItem('videoplayer-current-time');
+
+iframeVimeoPlayer
+  .setCurrentTime(playbackPosition)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.error(
+          'the time was less than 0 or greater than the video’s duration'
+        );
+        break;
+
+      default:
+        console.error('some other error occurred');
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+const throttledFnSaveFormData = throttle(e => {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}, 500);
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttledFnSaveFormData);
+
+populateForm();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log('formData:', formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateForm() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData) {
+    refs.email.value = savedData.email;
+    refs.message.value = savedData.message;
+  }
+}
