8a76918e27eedaa30449977997d2fdf0b02391af
VasylievYurii <122171281+VasylievYurii@users.noreply.github.com>
2023-05-12 23:41:04 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,15 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   email: document.querySelector('.feedback-form input'),
   message: document.querySelector('.feedback-form textarea'),
 };
+
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
+
 const throttledFnSaveFormData = throttle(e => {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
@@ -17,18 +20,30 @@ refs.form.addEventListener('input', throttledFnSaveFormData);
 
 populateForm();
 
-function onFormSubmit(evt) {
-  evt.preventDefault();
-  console.log('formData:', formData);
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+function populateForm() {
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (formData) {
+    if (formData.email) {
+      refs.email.value = formData.email;
+    }
+    if (formData.message) {
+      refs.message.value = formData.message;
+    }
+  }
 }
 
-function populateForm() {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  if (savedData) {
-    refs.email.value = savedData.email;
-    refs.message.value = savedData.message;
-  }
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  if (
+    refs.email.value.trim().length === 0 ||
+    refs.message.value.trim().length === 0
+  ) {   console.log('alert');
+    alert('Fill out the form before sending');
+  } else {
+    console.log('formData:', formData);
+    formData.email = '';
+    formData.message = '';
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+ }
 }
